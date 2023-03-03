3b74ed91209e10f3dec825a3463a84f9fa7daf32
Sohelahmed27 <sohel.gebcu@gmail.com>
2023-03-03 12:13:01 +0600

    feature-add

--- cosmic.js
+++ cosmic.js
@@ -22,8 +22,12 @@ const displayData =(tools)=>{
     <img src="${tool.image
     }" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <h5 class="card-title">Features</h5>
+      <ol>
+          <li>${tool.features[0]}</li>
+          <li>${tool.features[1]}</li>
+          <li>${tool.features[2]}</li>
+      </ol>
     </div>
     <div class="card-footer">
       <small class="text-muted">Last updated 3 mins ago</small>
